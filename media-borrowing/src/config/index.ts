f12f7f1186c9f1e8538f91d57b3aa41dad696daa
[CS-4] <[cameron.smith2406@gmail.com]>
2024-11-17 18:37:17 +0000

    Change max borrowing period to days.

--- media-borrowing/src/config/index.ts
+++ media-borrowing/src/config/index.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata'
 import { Container, Token } from 'typedi'
 
 export const MAX_RENEWALS = new Token<number>('MAX_RENEWALS');
-export const MAX_BORROWING_PERIOD_SECONDS = new Token<number>('MAX_BORROWING_PERIOD_SECONDS')
+export const MAX_BORROWING_PERIOD_DAYS = new Token<number>('MAX_BORROWING_PERIOD_DAYS')
 
 Container.set(MAX_RENEWALS, Number(process.env.MAX_RENEWALS) || 2)
-Container.set(MAX_BORROWING_PERIOD_SECONDS, Number(process.env.MAX_BORROWING_PERIOD_SECONDS) || 1209600)
+Container.set(MAX_BORROWING_PERIOD_DAYS, Number(process.env.MAX_BORROWING_PERIOD_SECONDS) || 14)
 
