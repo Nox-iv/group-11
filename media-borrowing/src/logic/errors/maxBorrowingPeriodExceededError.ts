3d880caf766fe716901fa2762fcc2724120c2771
[CS-4] <[cameron.smith2406@gmail.com]>
2024-11-17 18:32:35 +0000

    Add errors for borrowing period & renewal constraints.

---
+++ media-borrowing/src/logic/errors/maxBorrowingPeriodExceededError.ts
@@ -0,0 +1,7 @@
+export class MaxBorrowingPeriodExceededError extends Error {
+    constructor(message = 'Max borrowing period exceeded,') {
+        super(message)
+        Object.setPrototypeOf(this, MaxBorrowingPeriodExceededError.prototype);
+        this.name = 'MaxBorrowingPeriodExceededError.';
+    }
+}
--- media-borrowing/src/logic/index.ts
+++ media-borrowing/src/logic/index.ts
@@ -3,5 +3,15 @@ import { MediaBorrowingLogic } from "./mediaBorrowingLogic";
 import { MediaReturnLogic } from "./mediaReturnLogic";
 import { InvalidUserError } from "./errors/invalidUserError";
 import { InvalidBorrowingDateError } from "./errors/invalidBorrowingDateError";
+import { MaxRenewalsExceededError } from "./errors/maxRenewalsExceededError";
+import { MaxBorrowingPeriodExceededError } from "./errors/maxBorrowingPeriodExceededError";
 
-export { MediaBorrowingLogic, MediaReturnLogic, MediaBorrowingRecord, InvalidUserError, InvalidBorrowingDateError }
+export { 
+    MediaBorrowingLogic, 
+    MediaReturnLogic, 
+    MediaBorrowingRecord, 
+    InvalidUserError, 
+    InvalidBorrowingDateError,
+    MaxRenewalsExceededError,
+    MaxBorrowingPeriodExceededError
+}
