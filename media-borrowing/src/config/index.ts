b2a9c167c86b4df51add99a0ea92ecdfb790e793
[CS-4] <[cameron.smith2406@gmail.com]>
2024-11-18 11:27:34 +0000

    Config clean-up.

--- media-borrowing/src/config/index.ts
+++ media-borrowing/src/config/index.ts
@@ -1,9 +1,5 @@
-import 'reflect-metadata'
-import { Container, Token } from 'typedi'
 
-export const MAX_RENEWALS = new Token<number>('MAX_RENEWALS');
-export const MAX_BORROWING_PERIOD_DAYS = new Token<number>('MAX_BORROWING_PERIOD_DAYS')
+import { MAX_RENEWALS, MAX_BORROWING_PERIOD_DAYS } from "./config"
 
-Container.set(MAX_RENEWALS, Number(process.env.MAX_RENEWALS) || 2)
-Container.set(MAX_BORROWING_PERIOD_DAYS, Number(process.env.MAX_BORROWING_PERIOD_SECONDS) || 14)
+export {MAX_RENEWALS, MAX_BORROWING_PERIOD_DAYS}
 
