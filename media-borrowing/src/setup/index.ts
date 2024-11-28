7edc4790514de6161be8a0877b8dca21fa7b4a14
[CS-4] <[cameron.smith2406@gmail.com]>
2024-11-28 19:53:02 +0000

    Remove setup.

--- media-borrowing/src/setup/index.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata'
-import { Container } from 'typedi'
-import { MediaBorrowingRepository } from "../data";
-import { IMediaBorrowingRepository } from "../interfaces";
-
-export function setupDependencies() {
-    Container.set(IMediaBorrowingRepository, MediaBorrowingRepository)
-}
