ae6cedaccd261d44449ddae29deb881ea2140787
[CS-4] <[cameron.smith2406@gmail.com]>
2024-12-02 16:59:07 +0000

    Implement media borrowing config repository.

--- media-borrowing/src/mediaBorrowing/data/repositories/mediaBorrowingConfigRepository.ts
+++ media-borrowing/src/mediaBorrowing/data/repositories/mediaBorrowingConfigRepository.ts
@@ -4,14 +4,30 @@ import { IUnitOfWork } from "../../../db/interfaces/uow"
 import { IMediaBorrowingConfigRepository } from "../../interfaces/data/repositories"
 
 export class MediaBorrowingConfigRepository extends IMediaBorrowingConfigRepository {
-    constructor(@Inject() uow : IUnitOfWork) {
+    constructor(uow : IUnitOfWork) {
         super()
         this.uow = uow
     }
 
-    public async getRenewalLimit() : Promise<number | null> {
-        const result = new Message(false)
+    public async getRenewalLimit(branchId : number) : Promise<number | null> {
+        const connection = this.uow.getTransaction().getConnection()
+        const result = await connection.query<number>("SELECT renewalLimit FROM Branch_MediaBorrowingConfig WHERE branchId = $1", [branchId])
 
-        return new Promise(() => null)
+        if (result.length == 0) {
+            return null
+        }
+
+        return result[0]
+    }
+
+    public async getMaximumBorrowingDurationInDays(branchId : number) : Promise<number | null>  {
+        const connection = this.uow.getTransaction().getConnection()
+        const result = await connection.query<number>("SELECT maximumBorrowingDurationInDays FROM Branch_MediaBorrowingConfig WHERE branchId = $1", [branchId])
+
+        if (result.length == 0) {
+            return null
+        }
+
+        return result[0]
     }
 }
